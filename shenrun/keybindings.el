2f7fa37aa9ee892ee40a7f6434e8e83426fcb997
shenrun <1400010709@pku.edu.cn>
2017-02-10 01:33:16 +0800

    first

---
+++ shenrun/keybindings.el
@@ -0,0 +1,32 @@
+;;; keybindings.el --- Better Emacs Defaults Layer key bindings File
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(spacemacs/set-leader-keys "oc" 'org-capture)
+
+(defun occur-dwim ()
+  "call `occur' with a sane default."
+  (interactive)
+  (push (if (region-active-p)
+            (buffer-substring-no-properties
+             (region-beginning)
+             (region-end))
+          (let ((sym (thing-at-point 'symbol)))
+            (when (stringp sym)
+              (regexp-quote sym))))
+        regexp-history)
+  (call-interactively 'occur))
+
+(spacemacs/set-leader-keys "oo" 'occur-dwim)
+
+(spacemacs/set-leader-keys "oe" 'iedit-mode)
+
+(global-set-key (kbd "C-=") 'er/expand-region)
+ 
---
+++ shenrun/packages.el
@@ -0,0 +1,49 @@
+;;; packages.el --- shenrun layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: MacBook Air <macbookair@chenrundeMacBook-Air.local>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;; See the Spacemacs documentation and FAQs for instructions on how to implement
+;; a new layer:
+;;
+;;   SPC h SPC layers RET
+;;
+;;
+;; Briefly, each package to be installed or configured by this layer should be
+;; added to `shenrun-packages'. Then, for each package PACKAGE:
+;;
+;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
+;;   function `shenrun/init-PACKAGE' to load and initialize the package.
+
+;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
+;;   define the functions `shenrun/pre-init-PACKAGE' and/or
+;;   `shenrun/post-init-PACKAGE' to customize the package as it is loaded.
+
+;;; Code:
+
+(defconst shenrun-packages
+  '(youdao-dictionary
+    )
+  )
+
+(defun shenrun/init-youdao-dictionary()
+  (use-package youdao-dictionary
+    :defer t
+    :init
+    (spacemacs/set-leader-keys "oy" 'youdao-dictionary-search-at-point+)
+    )
+  )
+
+(defun shenrun/post-init-company()
+  (setq company-idle-delay 0.08
+        company-minimum-prefix-length 1)
+  )
+;;; packages.el ends here
